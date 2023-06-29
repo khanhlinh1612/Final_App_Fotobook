4faff0a1a0a1ac527ddaace94c22bc222a65af57
Kaly1612 <linhp069@gmail.com>
2023-06-29 17:11:09 +0700

    Add register

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'register/index'
+  root to: 'register#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  #
 end
