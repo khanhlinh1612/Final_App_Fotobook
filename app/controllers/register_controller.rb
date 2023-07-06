1af985542f9ae848acca9436cef8532658d1f7a2
Kaly1612 <linhp069@gmail.com>
2023-07-06 11:23:44 +0700

    add devise gem

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,4 +1,8 @@
 class RegisterController < ApplicationController
   def index
+    @user = User.new
+  end
+  def create
+    render plain: "Thanks"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  get '/user' => 'users#index'
-  root to: 'register#index'
+  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
